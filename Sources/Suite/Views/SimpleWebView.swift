b31ef666520d1af63974aa8213a21fc76c822c03
Ben Gottlieb <ben@standalone.com>
2024-02-02 05:23:04 -0600

    fixed some state update issues in SimpleWebView

--- Sources/Suite/Views/SimpleWebView.swift
+++ Sources/Suite/Views/SimpleWebView.swift
@@ -9,19 +9,23 @@ import SwiftUI
 import WebKit
 
 #if os(macOS)
-public struct SimpleWebViewWrapper: NSViewRepresentable {
-	let url: URL
+public struct SimpleWebView: NSViewRepresentable {
+	let request: URLRequest
 	
 	public init(url: URL) {
-		self.url = url
+		self.request = URLRequest(url: url)
 	}
 	
+	public init(request: URLRequest) {
+		self.request = request
+	}
+
 	public func updateNSView(_ uiView: NSViewType, context: Context) {
-		context.coordinator.webView.load(URLRequest(url: url))
+		load(request, into: context)
 	}
 	
 	public func makeNSView(context: Context) -> some NSView {
-		context.coordinator.webView.load(URLRequest(url: url))
+		load(request, into: context)
 		return context.coordinator.webView
 	}
 	
@@ -29,23 +33,19 @@ public struct SimpleWebViewWrapper: NSViewRepresentable {
 		Coordinator()
 	}
 	
-	public class Coordinator: NSObject, WKNavigationDelegate {
-		let webView: WKWebView
+	func load(_ request: URLRequest, into context: Context) {
+		if context.coordinator.loadedRequest == request { return }
 		
-		override init() {
-			webView = WKWebView()
-			super.init()
-			webView.navigationDelegate = self
-		}
+		context.coordinator.loadedRequest = request
+		context.coordinator.webView.load(request)
 	}
 }
 
 #endif
 
 #if os(iOS) || os(visionOS)
-public struct SimpleWebViewWrapper: UIViewRepresentable {
+public struct SimpleWebView: UIViewRepresentable {
 	let request: URLRequest
-	@State private var loadedRequest: URLRequest?
 	
 	public func updateUIView(_ uiView: UIViewType, context: Context) {
 		load(request, into: context)
@@ -65,18 +65,23 @@ public struct SimpleWebViewWrapper: UIViewRepresentable {
 	}
 	
 	func load(_ request: URLRequest, into context: Context) {
-		if loadedRequest == request { return }
+		if context.coordinator.loadedRequest == request { return }
 		
-		loadedRequest = request
+		context.coordinator.loadedRequest = request
 		context.coordinator.webView.load(request)
 	}
 	
 	public func makeCoordinator() -> Coordinator {
 		Coordinator()
 	}
-	
+}
+#endif
+
+#if os(iOS) || os(visionOS) || os(macOS)
+extension SimpleWebView {
 	public class Coordinator: NSObject, WKNavigationDelegate {
 		let webView: WKWebView
+		var loadedRequest: URLRequest?
 		
 		override init() {
 			webView = WKWebView()
