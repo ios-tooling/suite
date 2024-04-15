30bbb6a844dcf6656b7f33c9546a21f821c56e33
Ben Gottlieb <ben@standalone.com>
2024-04-15 05:33:51 -0500

    added Error.isFileNotFound

--- Sources/Studio/Foundation/Error.swift
+++ Sources/Studio/Foundation/Error.swift
@@ -13,3 +13,11 @@ public protocol DisplayableError: Error {
 	var errorTitle: String { get }
 	var errorMessage: String { get }
 }
+
+public extension Error {
+	var isFileNotFound: Bool {
+		let error = self as NSError
+		
+		return error.domain == NSCocoaErrorDomain && error.code == 260
+	}
+}
