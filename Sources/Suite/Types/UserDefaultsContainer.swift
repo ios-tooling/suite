0b66061ce070459aafd12ace64b653d2ad71b0be
Ben Gottlieb <ben@standalone.com>
2025-02-01 06:10:19 -0600

    improved AppSettings macro

--- Sources/Suite/Types/UserDefaultsContainer.swift
+++ Sources/Suite/Types/UserDefaultsContainer.swift
@@ -8,27 +8,27 @@
 import Foundation
 
 public protocol UserDefaultsContainer {
-	var userDefaults: UserDefaults { get }
+	nonisolated var userDefaults: UserDefaults { get }
 }
 
 public extension UserDefaultsContainer {
-	func hasValue(for key: String) -> Bool { userDefaults.object(forKey: key) != nil }
+	nonisolated func hasValue(for key: String) -> Bool { userDefaults.object(forKey: key) != nil }
 	
-	func setString(_ string: String?, for key: String) { userDefaults.setValue(string, forKey: key) }
-	func string(for key: String) -> String? { userDefaults.string(forKey: key) }
+	nonisolated func setString(_ string: String?, for key: String) { userDefaults.setValue(string, forKey: key) }
+	nonisolated func string(for key: String) -> String? { userDefaults.string(forKey: key) }
 	
-	func setBool(_ bool: Bool, for key: String) { userDefaults.setValue(bool, forKey: key) }
-	func bool(for key: String) -> Bool { userDefaults.bool(forKey: key) }
+	nonisolated func setBool(_ bool: Bool, for key: String) { userDefaults.setValue(bool, forKey: key) }
+	nonisolated func bool(for key: String) -> Bool { userDefaults.bool(forKey: key) }
 	
-	func setDate(_ date: Date?, for key: String) { userDefaults.setValue(date, forKey: key) }
-	func date(for key: String) -> Date? { userDefaults.object(forKey: key) as? Date }
+	nonisolated func setDate(_ date: Date?, for key: String) { userDefaults.setValue(date, forKey: key) }
+	nonisolated func date(for key: String) -> Date? { userDefaults.object(forKey: key) as? Date }
 	
-	func setData(_ data: Data?, for key: String) { userDefaults.setValue(data, forKey: key) }
-	func data(for key: String) -> Data? { userDefaults.data(forKey: key) }
+	nonisolated func setData(_ data: Data?, for key: String) { userDefaults.setValue(data, forKey: key) }
+	nonisolated func data(for key: String) -> Data? { userDefaults.data(forKey: key) }
 	
-	func setInt(_ int: Int, for key: String) { userDefaults.setValue(int, forKey: key) }
-	func int(for key: String) -> Int { userDefaults.integer(forKey: key) }
+	nonisolated func setInt(_ int: Int, for key: String) { userDefaults.setValue(int, forKey: key) }
+	nonisolated func int(for key: String) -> Int { userDefaults.integer(forKey: key) }
 	
-	func setDouble(_ double: Double, for key: String) { userDefaults.setValue(double, forKey: key) }
-	func double(for key: String) -> Double { userDefaults.double(forKey: key) }
+	nonisolated func setDouble(_ double: Double, for key: String) { userDefaults.setValue(double, forKey: key) }
+	nonisolated func double(for key: String) -> Double { userDefaults.double(forKey: key) }
 }
--- Sources/SuiteMacrosImpl/AppSettingsGenerator.ExtensionMacro.swift
+++ Sources/SuiteMacrosImpl/AppSettingsGenerator.ExtensionMacro.swift
@@ -13,8 +13,8 @@ import SwiftDiagnostics
 extension AppSettingsGenerator: ExtensionMacro {
 	public static func expansion(of node: SwiftSyntax.AttributeSyntax, attachedTo declaration: some SwiftSyntax.DeclGroupSyntax, providingExtensionsOf type: some SwiftSyntax.TypeSyntaxProtocol, conformingTo protocols: [SwiftSyntax.TypeSyntax], in context: some SwiftSyntaxMacros.MacroExpansionContext) throws -> [SwiftSyntax.ExtensionDeclSyntax] {
 		
-		guard declaration.as(ActorDeclSyntax.self) != nil else {
-			throw MacroFeedback.error("Macro `AppSettings` can only be applied to an actor")
+		guard declaration.as(ClassDeclSyntax.self) != nil else {
+			throw MacroFeedback.error("Macro `AppSettings` can only be applied to a class")
 		}
 		
 		var results: [ExtensionDeclSyntax] = []
