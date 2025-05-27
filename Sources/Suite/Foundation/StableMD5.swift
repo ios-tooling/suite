6fac24d1eea8df87107acfdd5c40d9d79d8cb8dd
Ben Gottlieb <ben@standalone.com>
2025-05-27 05:23:35 -0500

    md5 hashes now return throwing strings, not optionals

--- Sources/Suite/Foundation/StableMD5.swift
+++ Sources/Suite/Foundation/StableMD5.swift
@@ -7,76 +7,88 @@
 //
 
 public extension Encodable where Self: Decodable {
-	func stableMD5(using encoder: JSONEncoder = .default) -> String? {
-		guard let json: JSONDictionary = try? self.asJSON(using: encoder) else { return nil }
+	func stableMD5(using encoder: JSONEncoder = .default) throws -> String {
+		let json: JSONDictionary = try self.asJSON(using: encoder)
 		
-		return json.stableJSONMD5
+		return try json.stableJSONMD5
 	}
 }
 
 public extension JSONDictionary {
-	var stableJSONMD5: String? { stableMD5 }
-	var stableMD5: String? {
-		var keyHashes: [KeyHash] = []
-		
-		for (key, value) in self {
-			if let bool = value as? Bool {
-				keyHashes.append(.init(key: key, hash: String(describing: bool).md5))
-			} else if let int = value as? Int {
-				keyHashes.append(.init(key: key, hash: String(describing: int).md5))
-			} else if let string = value as? String {
-				keyHashes.append(.init(key: key, hash: string.md5))
-			} else if let float = value as? Float {
-				keyHashes.append(.init(key: key, hash: String(describing: float).md5))
-			} else if let double = value as? Double {
-				keyHashes.append(.init(key: key, hash: String(describing: double).md5))
-			} else if let date = value as? Date {
-				keyHashes.append(.init(key: key, hash: String(describing: date.timeIntervalSinceReferenceDate).md5))
-			} else if let data = value as? Data {
-				keyHashes.append(.init(key: key, hash: data.md5))
-			} else if let dict = value as? JSONDictionary {
-				keyHashes.append(.init(key: key, hash: dict.stableMD5))
-			} else if let array = value as? [(any JSONDataType)] {
-				keyHashes.append(.init(key: key, hash: array.stableMD5))
+	var stableJSONMD5: String { get throws { try stableMD5 } }
+	var stableMD5: String {
+		get throws {
+			var keyHashes: [KeyHash] = []
+			
+			for (key, value) in self {
+				if let bool = value as? Bool {
+					try keyHashes.append(.init(key: key, hash: String(describing: bool).md5))
+				} else if let int = value as? Int {
+					try keyHashes.append(.init(key: key, hash: String(describing: int).md5))
+				} else if let string = value as? String {
+					try keyHashes.append(.init(key: key, hash: string.md5))
+				} else if let float = value as? Float {
+					try keyHashes.append(.init(key: key, hash: String(describing: float).md5))
+				} else if let double = value as? Double {
+					try keyHashes.append(.init(key: key, hash: String(describing: double).md5))
+				} else if let date = value as? Date {
+					try keyHashes.append(.init(key: key, hash: String(describing: date.timeIntervalSinceReferenceDate).md5))
+				} else if let data = value as? Data {
+					try keyHashes.append(.init(key: key, hash: data.md5))
+				} else if let dict = value as? JSONDictionary {
+					try keyHashes.append(.init(key: key, hash: dict.stableMD5))
+				} else if let array = value as? [(any JSONDataType)] {
+					try keyHashes.append(.init(key: key, hash: array.stableMD5))
+				}
 			}
+			
+			let sorted = keyHashes.sorted()
+			let concat = sorted.reduce("") { combined, individual in combined + (individual.hash ?? "") }
+			
+			return try concat.md5
 		}
-		
-		let sorted = keyHashes.sorted()
-		let concat = sorted.reduce("") { combined, individual in combined + (individual.hash ?? "") }
-		
-		return concat.md5
+	}
+}
+
+enum StableMD5Error: LocalizedError { case unknownType(Sendable)
+	var errorDescription: String? {
+		switch self {
+		case .unknownType(let what): "Unknown JSON Data: \(what)"
+		}
 	}
 }
 
 public extension [any JSONDataType] {
-	var stableMD5: String? {
-		let md5s: [String] = self.compactMap { value -> String? in
-			if let bool = value as? Bool {
-				String(describing: bool).md5
-			} else if let int = value as? Int {
-				String(describing: int).md5
-			} else if let string = value as? String {
-				string.md5
-			} else if let float = value as? Float {
-				String(describing: float).md5
-			} else if let double = value as? Double {
-				String(describing: double).md5
-			} else if let date = value as? Date {
-				String(describing: date).md5
-			} else if let data = value as? Data {
-				data.md5
-			} else if let dict = value as? JSONDictionary {
-				dict.stableMD5
-			} else if let array = value as? [(any JSONDataType)] {
-				array.stableMD5
-			} else {
-				nil
+	var stableMD5: String {
+		get throws {
+			let md5s: [String] = try self.compactMap { value -> String in
+				if let bool = value as? Bool {
+					try String(describing: bool).md5
+				} else if let int = value as? Int {
+					try String(describing: int).md5
+				} else if let string = value as? String {
+					try string.md5
+				} else if let float = value as? Float {
+					try String(describing: float).md5
+				} else if let double = value as? Double {
+					try String(describing: double).md5
+				} else if let date = value as? Date {
+					try String(describing: date).md5
+				} else if let data = value as? Data {
+					try data.md5
+				} else if let dict = value as? JSONDictionary {
+					try dict.stableMD5
+				} else if let array = value as? [(any JSONDataType)] {
+					try array.stableMD5
+				} else {
+					throw StableMD5Error.unknownType(value)
+				}
 			}
+			
+			let concat = md5s.reduce("") { combined, individual in combined + individual }
+			
+			return try concat.md5
 		}
-		
-		let concat = md5s.reduce("") { combined, individual in combined + individual }
-		
-		return concat.md5
 	}
 }
 
