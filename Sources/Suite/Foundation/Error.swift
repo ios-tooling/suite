ba3e8fc3c13013513339f87ac2d5d74ed6215980
Ben Gottlieb <ben@standalone.com>
2025-06-24 18:04:02 -0500

    added Error.decodingDescription

--- Sources/Suite/Foundation/Error.swift
+++ Sources/Suite/Foundation/Error.swift
@@ -28,4 +28,33 @@ public extension Error {
 		
 		return error.code == 1001
 	}
+	
+	var decodingDescription: String? {
+		guard let error = self as? DecodingError else { return nil }
+		
+		return switch error {
+		case .typeMismatch(let key, let context):
+			"Type Mismatch \(key), path: \(context.pathDescription)"
+		case .valueNotFound(let key, let context):
+			"Value not Found error \(key), path: \(context.pathDescription)"
+		case .keyNotFound(let key, let context):
+			"Key not Found error \(key), path: \(context.pathDescription)"
+		case .dataCorrupted(let key):
+			"error \(key): \(error.localizedDescription)"
+		default:
+			"ERROR: \(error.localizedDescription)"
+		}
+	}
+}
+
+extension DecodingError.Context {
+	var pathDescription: String {
+		return codingPath.map( {
+			if let index = $0.intValue {
+				"[\(index)]"
+			} else {
+				$0.stringValue
+			}
+		}).joined(separator: ", ")
+	}
 }
