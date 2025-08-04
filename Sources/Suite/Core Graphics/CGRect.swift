699c5a037c1c26a8ecd3eaac7eb6cda827542ec2
Ben Gottlieb <ben@standalone.com>
2025-08-04 18:29:14 -0500

    added a retroactive to CGRect: Codable

--- Sources/Suite/Core Graphics/CGRect.swift
+++ Sources/Suite/Core Graphics/CGRect.swift
@@ -63,7 +63,7 @@ extension CGRect: StringInitializable {
 	}
 }
 
-extension CGRect.Placement: Codable {
+extension CGRect.Placement: @retroactive Codable {
     public enum PlacementError: Error, Sendable { case invalidIntegerValue, invalidStringValue, noValue }
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
