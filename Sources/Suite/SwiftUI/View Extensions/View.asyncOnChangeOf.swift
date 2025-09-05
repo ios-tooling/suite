891206cb9a71d4746727b336916593df430fd8a2
Ben Gottlieb <ben@standalone.com>
2025-09-05 05:22:13 -0500

    added View.asyncOnChange(of:)

---
+++ Sources/Suite/SwiftUI/View Extensions/View.asyncOnChangeOf.swift
@@ -0,0 +1,23 @@
+//
+//  View.asyncOnChangeOf.swift
+//  Suite
+//
+//  Created by Ben Gottlieb on 9/5/25.
+//
+
+import SwiftUI
+
+@available(iOS 17.0, *)
+public extension View {
+	@ViewBuilder func asyncOnChangeOf<T: Equatable>(of value: T, initial: Bool = false, file: StaticString = #file, line: UInt = #line, perform action: @escaping () async throws -> ()) -> some View {
+		onChange(of: value, initial: initial) {
+			Task {
+				do {
+					try await action()
+				} catch {
+					print("asyncOnChange of failed, \(file):\(line): \(error)")
+				}
+			}
+		}
+	}
+}
