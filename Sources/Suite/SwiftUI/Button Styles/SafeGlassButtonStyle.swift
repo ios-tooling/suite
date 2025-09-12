f933d95ce31c125d7951021a0f09f879b6de4979
Ben Gottlieb <ben@standalone.com>
2025-09-12 17:22:27 -0500

    added safeGlassButton

---
+++ Sources/Suite/SwiftUI/Button Styles/SafeGlassButtonStyle.swift
@@ -0,0 +1,31 @@
+//
+//  safeGlassButton.swift
+//  Suite
+//
+//  Created by Ben Gottlieb on 9/12/25.
+//
+
+import SwiftUI
+
+@available(iOS 15.0, macOS 13, watchOS 9, *)
+public extension View {
+	@ViewBuilder func safeGlassButton(prominent: Bool = false) -> some View {
+		if #available(iOS 26.0, *) {
+			if prominent {
+				self
+					.buttonStyle(.glassProminent)
+			} else {
+				self
+					.buttonStyle(.glass)
+			}
+		} else {
+			if prominent {
+				self
+					.buttonStyle(.borderedProminent)
+			} else {
+				self
+					.buttonStyle(.bordered)
+			}
+		}
+	}
+}
