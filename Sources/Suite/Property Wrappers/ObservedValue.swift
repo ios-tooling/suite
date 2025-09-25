2c816ff4806a3cab94911b2dfe92dd4c150fd9c1
Ben Gottlieb <ben@standalone.com>
2025-09-24 21:22:53 -0500

    added MutableObservedValue property wrapper

--- Sources/Suite/Property Wrappers/ObservedValue.swift
+++ Sources/Suite/Property Wrappers/ObservedValue.swift
@@ -25,3 +25,34 @@ import SwiftUI
 	}
 }
 
+@propertyWrapper @MainActor public struct MutableObservedValue<Target: ObservableObject & Sendable, Value: Sendable>: DynamicProperty, Sendable {
+	@ObservedObject var target: Target
+	@State public var wrappedValue: Value?
+	let get: @Sendable (Target) async -> Value
+	let set: @Sendable (Target, Value?) async -> Void
+
+	@MainActor public init(_ target: Target, get: @Sendable @escaping (Target) async -> Value, set: @Sendable @escaping (Target, Value?) async -> Void) {
+		self.target = target
+		self.get = get
+		self.set = set
+		update()
+	}
+	
+	nonisolated public func update() {
+		Task { @MainActor in
+			_wrappedValue.wrappedValue = await get(target)
+		}
+	}
+	
+	@MainActor public var projectedValue: Binding<Value?> {
+		.init(
+			get: { wrappedValue },
+			set: { value in
+				Task {
+					await self.set(self.target, value)
+				}
+			}
+		)
+	}
+}
+
