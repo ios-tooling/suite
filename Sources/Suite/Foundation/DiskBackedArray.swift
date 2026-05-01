8ac4eb26672c481cc855c0811523d4d0ce11a5aa
Ben Gottlieb <ben@standalone.com>
2026-05-01 09:25:46 -0500

    added DiskBackedArray and Dictionary

---
+++ Sources/Suite/Foundation/DiskBackedArray.swift
@@ -0,0 +1,59 @@
+//
+//  DiskBackedArray.swift
+//  Suite
+//
+//  Created by Ben Gottlieb on 5/1/26.
+//
+
+
+public struct DiskBackedArray<Value: Codable> {
+	let cacheURL: URL
+	let encoder: JSONEncoder
+	let decoder: JSONDecoder
+	
+	var cache: [Value] = []
+	
+	public subscript(_ index: Int) -> Value {
+		get { cache[index] }
+		set {
+			cache[index] = newValue
+			save()
+		}
+	}
+	
+	public init(cacheURL: URL, encoder: JSONEncoder = .init(), decoder: JSONDecoder = .init(), cache: [Value] = []) {
+		self.cacheURL = cacheURL
+		self.decoder = decoder
+		self.encoder = encoder
+		self.cache = cache
+		
+		try? FileManager.default.createDirectory(at: cacheURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+		if let data = try? Data(contentsOf: cacheURL) {
+			self.cache = (try? decoder.decode([Value].self, from: data)) ?? []
+		}
+	}
+	
+	func save() {
+		do {
+			let data = try encoder.encode(cache)
+			try data.write(to: cacheURL, options: .atomic)
+		} catch {
+			if #available(iOS 16, macOS 14, tvOS 16, watchOS 9, *) {
+				print("Failed to write [\(String(describing: Value.self)) to \(cacheURL.path(percentEncoded: false)): \(error.localizedDescription)")
+			} else {
+				print("Failed to write [\(String(describing: Value.self)) to \(cacheURL): \(error.localizedDescription)")
+			}
+		}
+	}
+}
+
+extension DiskBackedArray where Value: Equatable {
+	public subscript(_ index: Int) -> Value {
+		get { cache[index] }
+		set {
+			if cache[index] == newValue { return }
+			cache[index] = newValue
+			save()
+		}
+	}
+}
---
+++ Sources/Suite/Foundation/DiskBackedDictionary.swift
@@ -0,0 +1,71 @@
+//
+//  DiskBackedDictionary.swift
+//  Suite
+//
+//  Created by Ben Gottlieb on 5/1/26.
+//
+
+
+public struct DiskBackedDictionary<Key: Hashable & Codable, Value: Codable> {
+	let cacheURL: URL
+	let encoder: JSONEncoder
+	let decoder: JSONDecoder
+	
+	var cache: [Key: Value] = [:]
+	
+	public subscript(_ key: Key, `default` def: Value? = nil) -> Value? {
+		get { cache[key] ?? def }
+		set {
+			if let newValue {
+				cache[key] = newValue
+				save()
+			} else if cache[key] != nil {
+				cache.removeValue(forKey: key)
+				save()
+			}
+		}
+	}
+	
+	public init(cacheURL: URL, encoder: JSONEncoder = .init(), decoder: JSONDecoder = .init(), cache: [Key: Value] = [:]) {
+		self.cacheURL = cacheURL
+		self.decoder = decoder
+		self.encoder = encoder
+		self.cache = cache
+		
+		try? FileManager.default.createDirectory(at: cacheURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+		if let data = try? Data(contentsOf: cacheURL) {
+			self.cache = (try? decoder.decode([Key: Value].self, from: data)) ?? [:]
+		}
+	}
+	
+	func save() {
+		do {
+			let data = try encoder.encode(cache)
+			try data.write(to: cacheURL, options: .atomic)
+		} catch {
+			if #available(iOS 16, macOS 14, tvOS 16, watchOS 9, *) {
+				print("Failed to write [\(String(describing: Key.self)):\(String(describing: Value.self)) to \(cacheURL.path(percentEncoded: false)): \(error.localizedDescription)")
+			} else {
+				print("Failed to write [\(String(describing: Key.self)):\(String(describing: Value.self)) to \(cacheURL): \(error.localizedDescription)")
+			}
+		}
+	}
+}
+
+
+extension DiskBackedDictionary where Value: Equatable {
+	public subscript(_ key: Key, `default` def: Value? = nil) -> Value? {
+		get { cache[key] ?? def }
+		set {
+			if let newValue {
+				if cache[key] == newValue { return }
+				cache[key] = newValue
+				save()
+			} else if cache[key] != nil {
+				cache.removeValue(forKey: key)
+				save()
+			}
+		}
+	}
+
+}
